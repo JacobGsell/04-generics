7594b46beaaf6b42361b2d3d7a49570aca96c9f1
gsellja67901 <jacob.gsell@gmx.de>
2019-10-20 14:16:02 +0200

    Did some stuff

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,10 +1,14 @@
 package ohm.softa.a04;
 
-public interface SimpleList extends Iterable {
+import java.util.function.Function;
+
+
+
+public interface SimpleList<T> extends Iterable<T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T item);
 
 	/**
 	 * @return current size of the list
@@ -15,5 +19,25 @@ public interface SimpleList extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
+	default SimpleList<T> filter(SimpleFilter<T> filter) {
+        SimpleList result = new SimpleListImpl();
+        for(T t : this){
+            if(filter.include(t)){
+                result.add(t);
+            }
+        }
+        return result;
+    }
+
+	@SuppressWarnings("unchecked")
+	default <R> SimpleList<R> map(Function <T,R> func){
+
+		SimpleListImpl<R> result = new SimpleListImpl<>();
+
+		for(T t: this){
+			result.add(func.apply(t));
+		}
+
+		return result;
+	}
 }
